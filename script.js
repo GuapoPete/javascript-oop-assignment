beb89f99fd1686e22d2f7f6ebd8c9077cd301df4
Peter Fomunung <DeathFaresa@gmail.com>
2023-07-27 19:40:41 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -7,3 +7,35 @@
 // (This is called a 'getter' in Object Oriented Programming)
 
 // If you're stuck, take a look at the example class here for reference: https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/#
+class Car {
+  constructor(make, model, year, color) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+
+  getMake() {
+    return this.make;
+  }
+
+  getModel() {
+    return this.model;
+  }
+
+  getYear() {
+    return this.year;
+  }
+
+  getColor() {
+    return this.color;
+  }
+}
+
+let myCar = new Car("Toyota", "Camry", 2022, "Silver");
+
+console.log("Make:", myCar.getMake());   // Output: Make: Toyota
+console.log("Model:", myCar.getModel()); // Output: Model: Camry
+console.log("Year:", myCar.getYear());   // Output: Year: 2022
+console.log("Color:", myCar.getColor()); // Output: Color: Silver
+
